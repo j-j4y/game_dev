fe1876dd7944a78885cdcd410ede762756e0504a
j-j4y <166659170+j-j4y@users.noreply.github.com>
2026-09-08 13:59:23 -0600

    added bg music and new game bg

--- 2d-project-assets/player.gd
+++ 2d-project-assets/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
 signal health_depleted 
 var health = 100.0
-
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right", "move_up", "move_down")
