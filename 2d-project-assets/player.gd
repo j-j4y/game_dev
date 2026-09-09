5dae92807f278a6406ff8075bf46a9a928de5f58
j-j4y <166659170+j-j4y@users.noreply.github.com>
2026-09-09 14:01:16 -0600

    added damage multiplier to player after 50% HP

--- 2d-project-assets/player.gd
+++ 2d-project-assets/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 signal health_depleted 
-var health = 100.0
+var max_health = 100.0
+var health = max_health
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right", "move_up", "move_down")
@@ -15,9 +16,16 @@ func _physics_process(delta):
 		
 	 
 	const DAMAGE_RATE = 5.0
+	const EXTRA_DAMAGE_MULT = 2.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	
 	if overlapping_mobs.size() > 0:
-		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		var damage = DAMAGE_RATE * overlapping_mobs.size() * delta
+		if health <= max_health * 0.5:
+			damage *= EXTRA_DAMAGE_MULT
+		health -= damage
+		health = max(health,0)
 		%ProgressBar.value = health
+		
 		if health <= 0.0:
 			health_depleted.emit()
